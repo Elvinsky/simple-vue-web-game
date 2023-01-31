145f566ab8cb5b34115f70c87100a0bcb88c02f9
Elvinsky <mikhnevichnik@gmail.com>
2023-01-31 18:27:33 +0300

    attacks & round counter

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
+const HEALTH = 100;
+const controlRandom = (min, max) => {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
 const app = Vue.createApp({
   data() {
-    return {};
+    return {
+      playerHealth: HEALTH,
+      monsterHealth: HEALTH,
+      round: 0,
+    };
   },
-  methods: {},
+  methods: {
+    handleAttackMonster() {
+      this.round++;
+      const dmg = controlRandom(5, 12);
+      this.monsterHealth -= dmg;
+      this.handleAttackPlayer();
+    },
+    handleAttackPlayer() {
+      const dmg = controlRandom(8, 15);
+      this.playerHealth -= dmg;
+    },
+    handleSpecialAttackMonster() {
+      this.round++;
+      const dmg = controlRandom(10, 25);
+      this.monsterHealth -= dmg;
+      this.handleAttackPlayer();
+    },
+  },
 });
 app.mount("#game");
